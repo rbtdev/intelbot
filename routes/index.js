6a7f82f3d36dc08d6c1554d2276c85785d1a155d
Rob Thuleen <rob.thuleen@gmail.com>
2015-02-15 14:24:45 -0800

    Added commands

--- routes/index.js
+++ routes/index.js
@@ -15,22 +15,44 @@ router.get('/ingress', function (req,res) {
 router.post('/ingress',function(req,res) {
     var reply = slack.respond(req.body,function(hook) {
     	var links = [
-			{area: "EC", link:""},
-			{area: "IB", link:""},
-			{area: "PB", link:"https://www.ingress.com/intel?ll=32.791077,3122.712924&z=13"},
-			{area: "MMB", link:""}
+			{area: "ec", link:""},
+			{area: "ib", link:""},
+			{area: "pb", link:"https://www.ingress.com/intel?ll=32.791077,3122.712924&z=13"},
+			{area: "mmb", link:""}
 		];
-		var area = hook.text.split(' ')[1];
-		var link = "";
-		for (var i = 0; i<links.length; i++) {
-			if (links[i].area == area.toUpperCase()) {
-				link = links[i].link;
+		var input = hook.text.toLowerCase.split(' ');
+		var response = "";
+		if (input.length > 1) {
+			var command = input[1];
+			switch (command) {
+				case "help"
+					response = "Help text goes here";
+				break;
+				case "add":
+					if (input.lengh >= 4) {
+						var newArea = input[2];
+						var newLink = input[3];
+						response = "Adding " + newArea + " " + newLink;
+					}
+					else {
+						response = "please include an area and link";
+					}
+				break;
+				default:
+					response = "Area not found";
+					for (var i = 0; i<links.length; i++) {
+						if (links[i].area == input[1] {
+							response = links[i].link;
+							break;
+						}
+					}
 				break;
 			}
+			
 		}
 
         return {
-            text: link,
+            text: "Intel link for area '" + response,
             username: hook.trigger_word
         };
     });
